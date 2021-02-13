a36e207f0325a55d01025c378be3ec933566e81e
rodrigomfguedes <79021594+rodrigomfguedes@users.noreply.github.com>
2021-02-13 19:11:39 +0000

    Add files via upload

---
+++ MAIN.py
@@ -0,0 +1,8 @@
+import funcs
+
+funcs.func_one()
+a = funcs.func_two()
+print(len(a))
+#for i in range(a[0]):
+    #for j in range(a[0]):
+     #   print(a[i][j])
---
+++ funcs.py
@@ -0,0 +1,17 @@
+import math
+pi = math.pi
+e = math.e
+
+
+def func_one():
+    print('Pi is %.3f: ' % pi)
+    print('e is %.3f: ' % e)
+    print('Their sum is %.3f: ' % (e+pi))
+
+
+def func_two():
+    # print('This is the cosine of Pi: ' + str(math.cos(pi)))
+    a = [
+        [2, 3, 4], [4, 5]
+    ]
+    return a
